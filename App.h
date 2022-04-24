cab620f62d04704de2c4377312b3297d7e93f796
Splox <mark.keenan.leblanc@gmail.com>
2022-04-24 13:48:23 -0400

    Added HWND handles for all projected window elements to be utilized by calculator window. Added CreateCalculatorWindowElements declaration to recreate these elements, this is to be called on resizing of Calculator Window Allowing people to elements on screen, this will be usefull for extremly long function definitions.

--- App.h
+++ App.h
@@ -31,10 +31,46 @@ private:
 	static LRESULT CallOnVisualizerWindowEvent(HWND t_hwnd, UINT t_msg, WPARAM t_wparam, LPARAM t_lparam);
 	
 	void InitWindows();
-	HWND calculator_window;
-	HWND visualizer_window;
-
 	
+	#pragma region VisualizerWindow
+	HWND visualizer_window;
+	#pragma endregion
+	
+
+	#pragma region CalculatorWindow
+	HWND calculator_window;
+
+		#pragma region CalculatorWindowElements
+		void CreateCalculatorWindowElements();
+		UINT vertical_spacing;
+		UINT horizontal_spacing;
+		UINT element_height;
+
+		HWND xmin_label;
+		HWND xmin_edit_box;
+		HWND xmax_label;
+		HWND xmax_edit_box;
+
+		HWND ymin_label;
+		HWND ymin_edit_box;
+		HWND ymax_label;
+		HWND ymax_edit_box;
+
+		HWND xresolution_label;
+		HWND xresolution_edit_box;
+
+		HWND xresolution_label;
+		HWND xresolution_edit_box;
+
+		HWND point1_label;
+		HWND point1_selection_box; //we want to switch cursor and begin tracking mouse clicks for hit detection
+		HWND point2_label;
+		HWND point2_selection_box; //we want to switch cursor and begin tracking mouse clicks for hit detection
+
+		#pragma endregion
+
+
+	#pragma endregion
 
 	#pragma region EventHandlers
 
